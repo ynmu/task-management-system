fdbc70bb8839e7a057a99d2124de887aa66e1077
Yuning <mu.yun@northeastern.edu>
2024-11-15 01:40:57 -0800

    feat: add attendee endpoints

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,24 @@
 import express from 'express';
 import userRoutes from './routes/users';
 import eventRoutes from './routes/events';
+import attendeeRoutes from './routes/attendees';
+import cors from 'cors';
+
 
 const app = express();
-const PORT = process.env.PORT || 5921;
+const PORT = process.env.PORT || 6521;
 
-// Middleware for parsing JSON
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
 
 // Routes
 app.use('/users', userRoutes);
 app.use('/events', eventRoutes);
+app.use('/attendees', attendeeRoutes);
 
 // Start the server
 app.listen(PORT, () => {
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://localhost:5921/';
+export const API_BASE_URL = 'http://localhost:6521';
