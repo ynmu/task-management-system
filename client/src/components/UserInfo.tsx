fbdfcabf30bd0be13d73017068c06714ac97ce59
Yuning <mu.yun@northeastern.edu>
2024-11-15 10:43:06 -0800

    style: minor style changes to user info

--- client/src/components/UserInfo.tsx
+++ client/src/components/UserInfo.tsx
@@ -15,7 +15,9 @@ const UserInfo: React.FC<UserInfoProps> = ({ role, employeeNumber, userName, ong
           <img src={profilePic} alt="Profile" className="user-profile-pic" />
           <div className="user-info">
               <div className="user-info-label"><div className="user-info-label-item">User Role</div> {role}</div>
-              <div className="user-info-label"><div className="user-info-label-item">Employee Number</div> {employeeNumber}</div>
+              <div className="user-info-label">
+                <div className="user-info-label-item">Employee Number</div> {String(employeeNumber).padStart(5, '0')}
+              </div>
               <div className="user-info-label"><div className="user-info-label-item">User Name</div> {userName}</div>
               <div className="user-info-label"><div className="user-info-label-item">Ongoing Jobs in Queue</div> {ongoingJobsCount}</div>
           </div>
