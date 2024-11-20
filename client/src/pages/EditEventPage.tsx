5698f556f1e864dcaead16411b88c2e45c79d71b
MeilinNiu <112957438+MeilinNiu@users.noreply.github.com>
2024-11-20 02:59:46 -0800

    add EditEventPage

---
+++ client/src/pages/EditEventPage.tsx
@@ -0,0 +1,15 @@
+import EditEvent from "../components/EditEvent";
+
+const EditEventPage: React.FC = () => {
+
+  return (
+    <div>
+      <header className="page-header">
+        <h1>Edit Event</h1>
+      </header>
+      <EditEvent /> 
+    </div>
+  );
+};
+
+export default EditEventPage;
